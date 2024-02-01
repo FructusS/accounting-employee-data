d30842eaecbc72f8d9dd45361ed786be13684066
Mihkail Afanasev <afanasev10056@gmail.com>
2024-02-01 23:09:57 +0300

    feat: create entity for database

---
+++ src/Backend/AccountingEmployeeData.Data/Enities/EmployeeEntity.cs
@@ -0,0 +1,23 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AccountingEmployeeData.Data.Enities
+{
+    public class EmployeeEntity
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+
+        public Date Birthday { get; set; }
+
+        public string Address { get; set; }
+
+        public string Department { get; set; }
+
+
+        public string Description { get; set; }
+
+     
+    }
+}

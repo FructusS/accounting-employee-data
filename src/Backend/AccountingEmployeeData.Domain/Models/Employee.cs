2f70aa595399f4bd9819a32428ace1cd6c383088
Mihkail Afanasev <afanasev10056@gmail.com>
2024-02-02 11:02:01 +0300

    refactor: add Id field with Guid type

--- src/Backend/AccountingEmployeeData.Domain/Models/Employee.cs
+++ src/Backend/AccountingEmployeeData.Domain/Models/Employee.cs
@@ -2,6 +2,7 @@
 {
     public class Employee
     {
+        public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Patronymic { get; set; }

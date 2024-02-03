10a885ba4e997d1a9627df8a303a211670f44049
Mihkail Afanasev <afanasev10056@gmail.com>
2024-02-03 22:01:32 +0300

    style: add spaces

--- src/Backend/AccountingEmployeeData.Data/Entities/EmployeeEntity.cs
+++ src/Backend/AccountingEmployeeData.Data/Entities/EmployeeEntity.cs
@@ -3,8 +3,11 @@
     public class EmployeeEntity
     {
         public Guid Id { get; set; }
+        
         public string FirstName { get; set; }
+        
         public string LastName { get; set; }
+        
         public string Patronymic { get; set; }
 
         public DateOnly Birthday { get; set; }
@@ -12,10 +15,8 @@
         public string Address { get; set; }
 
         public string Department { get; set; }
-
-
+        
         public string Description { get; set; }
-
-     
+        
     }
 }

b04ed712c75521b866d4a8d9489bf1a057800737
Mihkail Afanasev <afanasev10056@gmail.com>
2024-02-04 23:53:07 +0300

    feat: added BaseViewModel class that implements the INotifyPropertyChanged interface

---
+++ src/Frontend/Desktop/AccountingEmployeeData.Desktop/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AccountingEmployeeData.Desktop.ViewModels;
+
+public class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}

2f66b6cb56fb6df0e03f9aa1fc4853b5edd02d21
Mihkail Afanasev <afanasev10056@gmail.com>
2024-02-08 22:39:42 +0300

    feat: add event for edit and add employee

--- src/Frontend/Desktop/AccountingEmployeeData.Desktop/Events/EventEmployeeList.cs
+++ src/Frontend/Desktop/AccountingEmployeeData.Desktop/Events/EventEmployeeList.cs
@@ -1,3 +1,4 @@
+using AccountingEmployeeData.Domain.Models;
 using Prism.Events;
 
 namespace AccountingEmployeeData.Desktop.Events;
@@ -5,4 +6,11 @@ namespace AccountingEmployeeData.Desktop.Events;
 internal class NavigateToCreateEmployeeEvent : PubSubEvent
 {
     
+}
+internal class NavigateToEmployeeListEvent : PubSubEvent
+{
+}
+
+internal class NavigateToEditEmployeeEvent : PubSubEvent<Employee>
+{
 }
